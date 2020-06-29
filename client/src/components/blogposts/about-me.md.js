c5c9aff74fb1176b429beda139ef8e6d26e85fb1
detols <mpsericpark@gmail.com>
2020-06-28 20:46:17 -0400

    Completed About Me content section of webpage

---
+++ client/src/components/blogposts/about-me.md.js
@@ -0,0 +1,42 @@
+export default `
+
+<img src="images/personal2.jpg"
+     alt="Personal Image"
+     style="float: left; margin-right: 25px; width: 30%; height: 30%" />
+
+# Hey!
+
+My name is **Eric Park**, though my friends call me **Min**, a short form to my Korean traditional name Min Seo.
+
+I am a **Software Engineering Co-op** student at the *University of Guelph*, expecting graduation in the Winter of 2023.
+
+## Personal
+
+I am a:
+
+- Student
+- Programmer
+- Dancer
+
+I'm not the most sociable person, but I'm always down to talk about anything with anybody as long as I'm not busy. I often engage in my own
+activities and maintain my productivity and abilities in consideration of my long-term goals, so I can seem pretty distant from everything.
+Otherwise, I'm often out dancing somewhere, taking class, freestyle-sessioning, or in rehearsal. I'll also be working on funny projects
+that can teach me some relevant skills with friends out of enjoyment.
+
+
+## Career
+
+I got into software development as I began considering my purpose in life. Through a lot of soul searching in the *COVID19 pandemic*,
+despite already being enrolled into the program, I determined that I wanted to be able to help people the best through software.
+Whether I go into something specific like cyber security or full-stack development is something I'm still determining by experiencing
+and learning continuously. However the purpose remains simple: be able to support, assist and protect people through a technological means.
+
+I am currently working on my personal website and other projects, preparing for my fall co-op work term, and maintaining my school studies
+so I can manage my degree and workload freely. I am excited to begin my first co-op position at *NCR* as a *Software Developer*, and plan to
+do my very best in learning from them and working with them.
+
+I am also pursuing a minor is Psychology due to its potential use in understanding people's needs and effectively what they want. 
+By understanding this, I hope to become more successful and influential in any development I take part in.
+
+
+`;
