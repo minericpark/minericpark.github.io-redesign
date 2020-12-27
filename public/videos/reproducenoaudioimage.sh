bc4b83c11ad0415fabd87b5572d49b149d0cb776
detols <mpsericpark@gmail.com>
2020-12-26 19:26:52 -0500

    Refactoring: Removed unnecessary shell file from repository

--- public/videos/reproducenoaudioimage.sh
+++
@@ -1,4 +0,0 @@
-input_file='./studiogoindol(itsokayevenifyoucantseetheend).mp4'
-output_file='./studiogoindol(itsokayevenifyoucantseetheend)-ns.mp4'
-
-ffmpeg -i $input_file -c copy -an $output_file
